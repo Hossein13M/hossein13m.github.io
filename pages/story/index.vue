0f6f965bd636b50a4447629a8a30a1957384966a
hossein13m <hossein13mousavi@gmail.com>
2019-10-29 23:49:56 +0330

    story page have been finished

---
+++ pages/story/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="story">
+    <div class="top-contact justify-content-center mt-5">
+      <h1
+        class="text-center"
+        style="    font-weight: 300;
+    font-size: 100px;"
+      >
+        My Story!
+      </h1>
+    </div>
+    <div class="story-part-one mt-5 mr-5">
+      <p>
+        Hi, I'm Hossein and I am 22! I am studying Atomic Physics but I'm more
+        into computers than Physics!
+      </p>
+      <p>
+        I have begun working in the IT field since 2017 when I was 20. back then
+        I was into networking since I have MCSE, I was implementing Microsoft
+        based networks and then when I moved into Linux I started to work on
+        Linux based network as well.
+      </p>
+      <p>
+        After that, I begin to work in a Fintech company as a Tech Support Help
+        Desk and after a short time, I get promoted to become Tech Support Lead
+        on that company.
+      </p>
+      <p>
+        In early 2019 I started realizing that I'm into programming as well so I
+        begin studying C# at first but I did not start learning seriously until
+        May 2019 when I have been transferred to be a part of Front End Team at
+        XEOPS (yeah right! I did start with C# but then I have decided to choose
+        Front end over backend).
+      </p>
+      <p>
+        In the last couple of months I have worked on three projects, one of
+        them is Angular and the other two are Vue.js. I haven't got any chance
+        to work with React.js but I will embrace the experience!
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.top-contact h1 {
+  font-weight: 300;
+  font-size: 100px;
+}
+.story-part-one p {
+  font-size: 19px;
+  font-family: 'Kalam', cursive;
+}
+</style>
