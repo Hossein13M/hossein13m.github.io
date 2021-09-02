ed7d018d93e104cedfb52e87c76a0c1078937bc0
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-09-02 18:19:06 +0430

    feat(tailwinds): add custom tailwinds colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
-const { colors } = require('tailwindcss/defaultTheme');
+const { colors: defaultColors } = require('tailwindcss/defaultTheme');
+
+const colors = {
+    ...defaultColors,
+    ...{
+        'custom-purple': {
+            500: '#242430',
+            900: '#20202A',
+        },
+        'custom-grey': {
+            100: '#FAFAFC',
+            500: '#8C8C8E',
+        },
+        'custom-yellow': {
+            500: '#FFC107',
+        },
+    },
+};
 
 module.exports = {
     prefix: '',
