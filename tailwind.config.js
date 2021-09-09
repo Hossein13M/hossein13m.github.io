6b96d87ae868d9a4cefaee645c6b4c2053e13205
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-09-09 20:26:08 +0430

    chore(tailwinds): configure tailwinds breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,14 @@ module.exports = {
     theme: {
         colors: colors,
         extend: {},
+        screen: {
+            xs: { min: '320px', max: '640px' },
+            sm: { min: '640px', max: '767px' },
+            md: { min: '768px', max: '1023px' },
+            lg: { min: '1024px', max: '1279px' },
+            xl: { min: '1280px', max: '1535px' },
+            '2xl': { min: '1536px' },
+        },
     },
     variants: {
         extend: {},
