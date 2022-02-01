44b8685b8ce86d6c5a000ad969a465965407f008
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-01 23:31:01 +0330

    fix(tailwinds)!: change tailwinds configuration for version 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,22 +21,19 @@ const colors = {
 module.exports = {
     prefix: '',
     important: true,
-    purge: {
-        enable: true,
-        content: ['./src/**/*.{html,ts}'],
-        preserveHtmlElements: false,
-    },
+    content: ['./src/**/*.{html,ts}'],
     darkMode: 'class',
     theme: {
-        colors: colors,
-        extend: {},
-        screen: {
-            xs: { min: '320px', max: '640px' },
-            sm: { min: '640px', max: '767px' },
-            md: { min: '768px', max: '1023px' },
-            lg: { min: '1024px', max: '1279px' },
-            xl: { min: '1280px', max: '1535px' },
-            '2xl': { min: '1536px' },
+        extend: {
+            colors: colors,
+            screen: {
+                xs: { min: '320px', max: '640px' },
+                sm: { min: '640px', max: '767px' },
+                md: { min: '768px', max: '1023px' },
+                lg: { min: '1024px', max: '1279px' },
+                xl: { min: '1280px', max: '1535px' },
+                '2xl': { min: '1536px' },
+            },
         },
     },
     variants: {
