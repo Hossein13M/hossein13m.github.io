7b7e56230c88eb974698450aa9e8b472e194dac2
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-01 22:39:23 +0330

    feat(appModule)!: implement lazy loading in app module and app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
