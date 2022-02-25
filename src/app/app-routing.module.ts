618d01aac18821e37365007b11bf2ef8d99c9fc1
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-25 18:35:11 +0330

    refactor(appRouting): change root page from 'home' to ''

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
     },
 ];
