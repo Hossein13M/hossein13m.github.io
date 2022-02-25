ecbf3ecabb98b20a917253375f8052c3805cf26b
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-25 20:24:30 +0330

    feat(appRouting): implement lazy loading for other modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,31 @@ const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
-        data: { pageTitle: 'Home' },
+        data: { pageTitle: 'Home!' },
+    },
+    {
+        path: 'about-me',
+        loadChildren: () => import('./modules/about/about.module').then((m) => m.AboutModule),
+        data: { pageTitle: 'About Me!' },
+    },
+    {
+        path: 'contact-me',
+        loadChildren: () => import('./modules/contact/contact.module').then((m) => m.ContactModule),
+        data: { pageTitle: 'Contact Me!' },
+    },
+    {
+        path: 'publications',
+        loadChildren: () => import('./modules/publications/publications.module').then((m) => m.PublicationsModule),
+        data: { pageTitle: 'Publications!' },
+    },
+    {
+        path: 'skills',
+        loadChildren: () => import('./modules/skills/skills.module').then((m) => m.SkillsModule),
+        data: { pageTitle: 'Skills!' },
+    },
+    {
+        path: '**',
+        redirectTo: '',
     },
 ];
 
