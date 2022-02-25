860ccdcdff17e9f84738ed60d47cf5ca5bd5da59
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-02-25 20:24:58 +0330

    feat(navigationRoutes): change icon for about-me title

--- src/app/const/navigationRoutes.ts
+++ src/app/const/navigationRoutes.ts
@@ -2,7 +2,7 @@ import { NavigationRouteModel } from '../models/navigationRoute.model';
 
 export const NavigationRoutes: Array<NavigationRouteModel> = [
     { routeTitle: 'Home!', routeUrl: 'home', routeIcon: 'home' },
-    { routeTitle: 'About Me!', routeUrl: 'about', routeIcon: 'lightbulb' },
+    { routeTitle: 'About Me!', routeUrl: 'about-me', routeIcon: 'person' },
     { routeTitle: 'Contact Me!', routeUrl: 'contact-me', routeIcon: 'phone_iphone' },
     { routeTitle: 'Publications!', routeUrl: 'publications', routeIcon: 'article' },
     { routeTitle: 'Skills!', routeUrl: 'skills', routeIcon: 'psychology' },
