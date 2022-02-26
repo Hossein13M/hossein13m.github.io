d0d8db7252eb954d026e337110fa0cfb5e2044e8
hossein <hossein13mousavi@gmail.com>
2022-02-26 11:46:09 +0330

    feat(publication): initialize publication item

---
+++ src/app/components/publication-item/publication-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-publication-item',
+    templateUrl: './publication-item.component.html',
+    styleUrls: ['./publication-item.component.scss'],
+})
+export class PublicationItemComponent {
+    constructor() {}
+}
