1abc036a1958a74a25980109e4420fed479028ca
hossein <hossein13mousavi@gmail.com>
2022-03-07 16:52:20 +0330

    feat(pageHeader): get page-header's title from input and remove extra codes

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -1,23 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-page-header',
     templateUrl: './page-header.component.html',
     styleUrls: ['./page-header.component.scss'],
 })
-export class PageHeaderComponent implements OnInit {
+export class PageHeaderComponent {
     public backgroundImagePath: string = 'space11.png';
-    public pageTitle!: string;
     public publicationSubject: Array<string> = ['JavaScript.', 'TypeScript.', 'Angular.', 'Git.', 'Project Management.', 'Node.JS.'];
+    @Input() pageTitle!: string;
 
-    constructor(private readonly activatedRoute: ActivatedRoute) {}
-
-    ngOnInit(): void {
-        this.getCurrentRouteTitle();
-    }
-
-    private getCurrentRouteTitle(): void {
-        this.activatedRoute.data.subscribe((data) => (this.pageTitle = data['pageTitle']));
-    }
+    constructor() {}
 }
