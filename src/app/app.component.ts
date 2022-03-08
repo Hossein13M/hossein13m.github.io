beee98b6e0a26ab7f9070a5da40cac6f9357ff6c
hossein <hossein13mousavi@gmail.com>
2022-03-08 16:45:38 +0330

    fix(global)!: fix problem with window object in server side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { ActivationStart, Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { ProgressSpinner } from './components/progress-spinner/progress-spinner.model';
 import { Languages } from './const/languages';
 import { NavigationRoutes } from './const/navigationRoutes';
 import { NavigationRouteModel } from './models/navigationRoute.model';
+import { AppService } from './services/app.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -14,17 +16,18 @@ import { NavigationRouteModel } from './models/navigationRoute.model';
 export class AppComponent implements OnInit {
     public isDarkTheme: boolean = true;
     public languagesList: Array<ProgressSpinner> = [];
-    public screenWidth: number = window.innerWidth;
+    public screenWidth: number = this.appService.windowInnerWidth;
     public pageTitle!: string;
     public navigationRoutes: Array<NavigationRouteModel> = NavigationRoutes;
+
     @ViewChild('sidenav') sidenav!: MatSidenav;
     @ViewChild('infoSidenav') infoSidenav!: MatSidenav;
 
-    constructor(private readonly router: Router) {}
+    constructor(private readonly router: Router, private readonly appService: AppService, @Inject(PLATFORM_ID) private platformId: unknown) {}
 
     @HostListener('window:resize', ['$event'])
     public onResize() {
-        this.screenWidth = window.innerWidth;
+        this.screenWidth = this.appService.windowInnerWidth;
     }
 
     public navigateToOtherPages(destinationRoute: string): void {
@@ -32,9 +35,13 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.isDarkTheme = localStorage.getItem('theme') === 'dark';
+        this.applyChangesOnBrowserOnly(() => (this.isDarkTheme = localStorage.getItem('theme') === 'dark'));
         this.prepareLanguageListForSpinner();
         this.getCurrentRouteTitle();
+    }
+
+    private applyChangesOnBrowserOnly(callback: () => void): void {
+        isPlatformBrowser(!!this.platformId) && callback();
     }
 
     private getCurrentRouteTitle(): void {
@@ -45,7 +52,7 @@ export class AppComponent implements OnInit {
 
     public changeTheme(): void {
         this.isDarkTheme = !this.isDarkTheme;
-        localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'light');
+        this.applyChangesOnBrowserOnly(() => localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'light'));
     }
 
     private prepareLanguageListForSpinner(): void {
