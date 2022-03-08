277513a712bf2c5c36c7ec5c58bceda4ccd7eb5f
hossein <hossein13mousavi@gmail.com>
2022-03-08 16:43:50 +0330

    feat(global)!: implement angular universal for SSR purposes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,7 +34,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
