90f7ae6b0ef7b192090e4c186a3753f91a790e93
hossein <hossein13mousavi@gmail.com>
2022-03-08 16:46:06 +0330

    fix(navigationRoutes): add routeUrl of '/' for home page

--- src/app/const/navigationRoutes.ts
+++ src/app/const/navigationRoutes.ts
@@ -2,6 +2,7 @@ import { NavigationRouteModel } from '../models/navigationRoute.model';
 
 export const NavigationRoutes: Array<NavigationRouteModel> = [
     { routeTitle: 'Home!', routeUrl: 'home', routeIcon: 'home' },
+    { routeTitle: 'Home!', routeUrl: '', routeIcon: 'home' },
     { routeTitle: 'About Me!', routeUrl: 'about-me', routeIcon: 'person' },
     { routeTitle: 'Contact Me!', routeUrl: 'contact-me', routeIcon: 'phone_iphone' },
     { routeTitle: 'Publications!', routeUrl: 'publications', routeIcon: 'article' },
