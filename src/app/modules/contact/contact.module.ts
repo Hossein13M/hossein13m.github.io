942f4de74e6679d43b31dfab79b8e14bd61ea678
hossein <hossein13mousavi@gmail.com>
2022-03-09 15:38:38 +0330

    feat(contact): implement contact me form and customize its style

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { ContactRoutingModule } from './contact.routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [ContactComponent],
-    imports: [CommonModule, ContactRoutingModule],
+    imports: [CommonModule, ContactRoutingModule, MatIconModule, MatCardModule, MatDividerModule, MatButtonModule],
 })
 export class ContactModule {}
