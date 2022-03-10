e32b9264addf23dd54d4ec1056ae2410cef86f22
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-03-10 15:30:35 +0330

    fix(const): remove extra line of code from navigation route

--- src/app/const/navigationRoutes.ts
+++ src/app/const/navigationRoutes.ts
@@ -2,7 +2,6 @@ import { NavigationRouteModel } from '../models/navigationRoute.model';
 
 export const NavigationRoutes: Array<NavigationRouteModel> = [
     { routeTitle: 'Home!', routeUrl: '', routeIcon: 'home' },
-    // { routeTitle: 'Home!', routeUrl: 'home', routeIcon: 'home' },
     { routeTitle: 'About Me!', routeUrl: 'about-me', routeIcon: 'person' },
     { routeTitle: 'Contact Me!', routeUrl: 'contact-me', routeIcon: 'phone_iphone' },
     { routeTitle: 'Publications!', routeUrl: 'publications', routeIcon: 'article' },
