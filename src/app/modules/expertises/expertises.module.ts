47475b912ee15510ff0a88c1a2d4a5c33381a254
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-03-11 19:31:07 +0330

    feat(expertiseCard): implement expertise module

--- src/app/modules/expertises/expertises.module.ts
+++ src/app/modules/expertises/expertises.module.ts
@@ -2,9 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExpertisesComponent } from './expertises.component';
 import { ExpertisesRoutingModule } from './expertises.routing.module';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { ExpertiseCardModule } from '../../components/expertise-card/expertise-card.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [ExpertisesComponent],
-    imports: [CommonModule, ExpertisesRoutingModule],
+    imports: [
+        CommonModule,
+        ExpertisesRoutingModule,
+        MatTabsModule,
+        MatIconModule,
+        ExpertiseCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+    ],
 })
 export class ExpertisesModule {}
