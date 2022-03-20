611a170a0ab5e18c5a90d2929e3129410db032c3
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-03-21 01:16:59 +0330

    feat(home): implement home page

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HomeModel } from '../../models/home.model';
+import { HomeTitles } from '../../const/home';
 
 @Component({
     selector: 'app-home',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+    public headerFooterTitles: Array<HomeModel> = HomeTitles;
+
     constructor() {}
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
+import { HomeHeaderModule } from '../../components/home-header/home-header.module';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, RouterModule, HomeRoutingModule],
+    imports: [CommonModule, RouterModule, HomeRoutingModule, HomeHeaderModule, MatDividerModule],
     exports: [HomeComponent],
 })
 export class HomeModule {}
