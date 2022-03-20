3d2d8f8865aaf2d2d98a21d1fd92cbc7b1b49b3c
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-03-20 10:23:00 +0330

    chore(baseInfo): change base info QA section

--- src/app/const/baseInfo.ts
+++ src/app/const/baseInfo.ts
@@ -1,5 +1,6 @@
 export const BaseInfo: Array<{ subject: string; answer: string }> = [
     { subject: 'Occupation', answer: 'Software Developer' },
-    { subject: 'Specialization', answer: 'JavaScript' },
+    { subject: 'Specialization', answer: 'Web Development' },
+    { subject: 'Language', answer: 'TypeScript - JavaScript' },
     { subject: 'Age', answer: '25' },
 ];
