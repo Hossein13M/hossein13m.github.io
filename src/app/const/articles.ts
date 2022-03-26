f7c8d927a7280ff1c082e1fe3bf06fc629a0d8d9
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-03-26 22:49:37 +0430

    chore(publication): enhance publications photos

--- src/app/const/articles.ts
+++ src/app/const/articles.ts
@@ -4,7 +4,7 @@ export const Articles: Array<ArticlesModel> = [
     {
         title: 'Multiple Interceptors in Angular',
         url: 'https://medium.com/codex/multiple-interceptors-in-angular-e0880b2f7d91',
-        imgUrl: 'https://miro.medium.com/max/1400/0*aR4IMRSoPyOUIARq',
+        imgUrl: '../../../assets/images/articles/medium-interceptor.webp',
         summary:
             'One of the amazing features that Angular provides is the interceptors, but what does an Interceptor do, and can we implement multiple of them in our Angular project?',
         publisher: 'Medium',
@@ -13,7 +13,7 @@ export const Articles: Array<ArticlesModel> = [
     {
         title: 'Husky 6 Lint (prettier + eslint) and commitlint for JavaScript Projects',
         url: 'https://medium.com/codex/multiple-interceptors-in-angular-e0880b2f7d91',
-        imgUrl: 'https://miro.medium.com/max/1400/1*YXRxj1-SEHdrIpGNxVJvQw.jpeg',
+        imgUrl: '../../../assets/images/articles/medium-husky.webp',
         summary:
             'Programming is a teamwork job so we must assure that our codebase is clean and usable for everyone in the team with the team culture and the best practices.',
         publisher: 'Medium',
@@ -22,7 +22,7 @@ export const Articles: Array<ArticlesModel> = [
     {
         title: 'Angular Reactive Form including Angular Material and Custom Validation',
         url: 'https://medium.com/codex/multiple-interceptors-in-angular-e0880b2f7d91',
-        imgUrl: 'https://miro.medium.com/max/1400/0*0pTmoggTq4asbb0g',
+        imgUrl: '../../../assets/images/articles/medium-form.webp',
         summary:
             'Forms are major parts of every Angular project and in this article, we want to implement a Reactive Angular form with a custom and dynamic validator.',
         publisher: 'Medium',
@@ -31,7 +31,7 @@ export const Articles: Array<ArticlesModel> = [
     {
         title: 'Take a good look at filter, map, and reduce in JavaScript',
         url: 'https://medium.com/codex/multiple-interceptors-in-angular-e0880b2f7d91',
-        imgUrl: 'https://miro.medium.com/max/1400/0*wi_z1ghVqydGQRTM',
+        imgUrl: '../../../assets/images/articles/medium-javascript.webp',
         summary:
             'In JavaScript, we have these three methods as a part of Array.prototype method, but what are the differences between them, and what do they exactly do? so let’s dive into them!',
         publisher: 'Medium',
@@ -40,7 +40,7 @@ export const Articles: Array<ArticlesModel> = [
     {
         title: 'Touchpad gestures in Linux (Persian)',
         url: 'https://vrgl.ir/UPh81',
-        imgUrl: 'https://files.virgool.io/upload/users/45582/posts/gqcr3hgu3mqy/6wpmqilnlzid.jpeg',
+        imgUrl: '../../../assets/images/articles/virgool-touchpad.webp',
         summary:
             "Unlike Windows or macOS, which provides us with great touchpad gestures, Linux does not. So let's implement touchpad gestures and configure them together.",
         publisher: 'Virgool',
@@ -49,7 +49,7 @@ export const Articles: Array<ArticlesModel> = [
     {
         title: 'Introduction to API Blueprint',
         url: 'https://vrgl.ir/UPh81',
-        imgUrl: '../../../assets/images/apiBlueprint.jpg',
+        imgUrl: '../../../assets/images/articles/testfully-apiBlueprint.webp',
         summary: 'API blueprint is a powerful high-level API design language for web APIs.',
         publisher: 'Testfully',
         publicationUrl: 'https://testfully.io/blog',
@@ -57,7 +57,7 @@ export const Articles: Array<ArticlesModel> = [
     {
         title: 'Top 7 Free & Paid mock API tools (2022 Review)',
         url: 'https://testfully.io/blog/mock-api/',
-        imgUrl: '../../../assets/images/mockAPI.jpg',
+        imgUrl: '../../../assets/images/articles/testfully-mockAPI.webp',
         summary:
             'Sometimes called a fake API, A Mock API is when you build an API that returns the desired data. Still, it is not your actual API, and it all has been simulated for some use cases.',
         publisher: 'Testfully',
