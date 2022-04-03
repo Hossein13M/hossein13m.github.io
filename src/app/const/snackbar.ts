2893b10dd097f21eac2499e4d326506d5295c10c
hossein <hossein13mousavi@gmail.com>
2022-04-03 14:38:46 +0430

    feat(snackBar): implement snackbar for form submission

---
+++ src/app/const/snackbar.ts
@@ -0,0 +1,12 @@
+import { SnackBar } from '../models/snackbar.model';
+
+export const Snackbar: SnackBar = {
+    onSuccess: {
+        classes: ['bg-custom-yellow-500', 'text-custom-purple-500', 'font-bold'],
+        message: 'Your Message Has Been Sent',
+    },
+    onError: {
+        classes: ['bg-red-500', 'text-black', 'font-bold'],
+        message: 'There Was a Problem. Please Check The Form And Try Again!',
+    },
+};
