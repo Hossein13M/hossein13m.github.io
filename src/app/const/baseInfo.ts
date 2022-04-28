fa573674e03222f09805f0dfeb36d744244e43dc
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-04-28 11:00:00 +0430

    chore(baseInfo): change baseInfo language to be plural

--- src/app/const/baseInfo.ts
+++ src/app/const/baseInfo.ts
@@ -1,6 +1,6 @@
 export const BaseInfo: Array<{ subject: string; answer: string }> = [
     { subject: 'Occupation', answer: 'Software Developer' },
     { subject: 'Specialization', answer: 'Web Development' },
-    { subject: 'Language', answer: 'TypeScript - JavaScript' },
+    { subject: 'Languages', answer: 'TypeScript - JavaScript' },
     { subject: 'Age', answer: '25' },
 ];
