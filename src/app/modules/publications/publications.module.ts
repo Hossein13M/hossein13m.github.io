e41532412d16288d1c06921ec9f72e382f5006a5
hossein <hossein13mousavi@gmail.com>
2022-04-30 14:36:20 +0430

    feat(podcast)!: implement podcast component inside publications page

--- src/app/modules/publications/publications.module.ts
+++ src/app/modules/publications/publications.module.ts
@@ -4,9 +4,10 @@ import { PublicationsComponent } from './publications.component';
 import { PublicationsRoutingModule } from './publications.routing.module';
 import { PageHeaderModule } from '../../components/page-header/page-header.module';
 import { PublicationItemModule } from '../../components/publication-item/publication-item.module';
+import { PodcastModule } from '../../components/podcast/podcast.module';
 
 @NgModule({
     declarations: [PublicationsComponent],
-    imports: [CommonModule, PublicationsRoutingModule, PageHeaderModule, PublicationItemModule],
+    imports: [CommonModule, PublicationsRoutingModule, PageHeaderModule, PublicationItemModule, PodcastModule],
 })
 export class PublicationsModule {}
