229f7069370f96c1e5d58339fad19f88d536113e
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-05-31 21:50:51 +0430

    refactor(appInit): remove unused variables and arguments

--- src/app/services/app-init.service.ts
+++ src/app/services/app-init.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
-import { Subject } from 'rxjs';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { LoadingSpinnerComponent } from '../components/loading-spinner/loading-spinner.component';
 
 @Injectable({ providedIn: 'root' })
 export class AppInitService {
     private overlayRef?: OverlayRef;
-    public isLoading = new Subject<boolean>();
     constructor(private overlay: Overlay) {}
 
     Init() {
-        return new Promise<void>((resolve, reject) => {
+        return new Promise<void>((resolve) => {
             this.overlayRef = this.overlay.create({
                 panelClass: ['bg-custom-purple-500', 'w-full', 'h-full', 'flex', 'justify-center'],
                 hasBackdrop: true,
