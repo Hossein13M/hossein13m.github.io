eac3e92c36a81a2e4363233166a79444e9be95bf
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-05-31 21:50:03 +0430

    feat(global)!: implement loading bar with angular APP_INITIALIZER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -17,6 +17,13 @@ import { GoogleAnalyticsService } from './services/google-analytics.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LoadingSpinnerModule } from './components/loading-spinner/loading-spinner.module';
+import { AppInitService } from './services/app-init.service';
+
+export function initializeApp(appInitService: AppInitService) {
+    return (): Promise<any> => {
+        return appInitService.Init();
+    };
+}
 
 @NgModule({
     declarations: [AppComponent],
@@ -38,7 +45,7 @@ import { LoadingSpinnerModule } from './components/loading-spinner/loading-spinn
         MatTooltipModule,
         LoadingSpinnerModule,
     ],
-    providers: [GoogleAnalyticsService],
+    providers: [GoogleAnalyticsService, { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [AppInitService], multi: true }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
