5fa0074402cf0d6d8904137441d92ebf2db5f15c
hossein <hossein13mousavi@gmail.com>
2022-06-01 09:50:45 +0430

    chore(global)!: remove loading bar for now!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -17,13 +17,6 @@ import { GoogleAnalyticsService } from './services/google-analytics.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LoadingSpinnerModule } from './components/loading-spinner/loading-spinner.module';
-import { AppInitService } from './services/app-init.service';
-
-// export function initializeApp(appInitService: AppInitService) {
-//     return (): Promise<any> => {
-//         return appInitService.Init();
-//     };
-// }
 
 @NgModule({
     declarations: [AppComponent],
@@ -45,10 +38,7 @@ import { AppInitService } from './services/app-init.service';
         MatTooltipModule,
         LoadingSpinnerModule,
     ],
-    providers: [
-        GoogleAnalyticsService,
-        // { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [AppInitService], multi: true }
-    ],
+    providers: [GoogleAnalyticsService],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,24 +1,14 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { AppService } from './services/app.service';
 import { WindowService } from './services/window.service';
-import { AppInitService } from './services/app-init.service';
-
-export function initializeApp(appInitService: AppInitService) {
-    return (): Promise<any> => {
-        return appInitService.Init();
-    };
-}
 
 @NgModule({
     imports: [AppModule, ServerModule],
     bootstrap: [AppComponent],
-    providers: [
-        { provide: AppService, useClass: WindowService },
-        { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [AppInitService], multi: true },
-    ],
+    providers: [{ provide: AppService, useClass: WindowService }],
 })
 export class AppServerModule {}
