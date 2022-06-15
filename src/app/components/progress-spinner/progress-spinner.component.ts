2b782e974c5b7e2a6585cb62f66cd71cb03e3039
hossein <hossein13mousavi@gmail.com>
2022-06-15 08:55:54 +0430

    refactor(global): transfer progress spinner model to models directory

--- src/app/components/progress-spinner/progress-spinner.component.ts
+++ src/app/components/progress-spinner/progress-spinner.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { ProgressSpinner } from './progress-spinner.model';
+import { ProgressSpinner } from '../../models/progress-spinner.model';
 
 @Component({
     selector: 'app-progress-spinner',
