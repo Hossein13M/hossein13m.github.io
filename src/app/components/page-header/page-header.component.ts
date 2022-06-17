621a3f0dbd868af28ba447967e293667177c3c11
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-06-17 15:41:57 +0430

    refactor(components): remove pageHeader template and scss

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -3,8 +3,12 @@ import { NavigationRouteModel } from '../../models/navigationRoute.model';
 
 @Component({
     selector: 'app-page-header',
-    templateUrl: './page-header.component.html',
-    styleUrls: ['./page-header.component.scss'],
+    template: `
+        <header class="flex items-center mb-5 p-5 dark:bg-custom-yellow-500 bg-custom-blue-600 dark:text-custom-purple-900 text-yellow-500">
+            <mat-icon class="mx-5 flex items-center text-4xl">{{ pageHeaderInfo.routeIcon }}</mat-icon>
+            <h1 class="m-0 text-4xl">{{ pageHeaderInfo.routeTitle }}</h1>
+        </header>
+    `,
 })
 export class PageHeaderComponent {
     public backgroundImagePath: string = 'space11.png';
