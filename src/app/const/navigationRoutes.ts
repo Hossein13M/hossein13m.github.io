8e822e9ff5bcd4de735409e6e08b0b40ba129a01
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-06-19 00:46:37 +0430

    chore(navConst): change project to experiences in nav route const

--- src/app/const/navigationRoutes.ts
+++ src/app/const/navigationRoutes.ts
@@ -5,5 +5,5 @@ export const NavigationRoutes: Array<NavigationRouteModel> = [
     { routeTitle: 'Contact Me!', routeUrl: 'contact-me', routeIcon: 'phone_iphone' },
     { routeTitle: 'Publications!', routeUrl: 'publications', routeIcon: 'article' },
     { routeTitle: 'Expertises!', routeUrl: 'expertises', routeIcon: 'psychology' },
-    // { routeTitle: 'Projects!', routeUrl: 'projects', routeIcon: 'design_services' },
+    // { routeTitle: 'Projects!', routeUrl: 'experiences', routeIcon: 'design_services' },
 ];
