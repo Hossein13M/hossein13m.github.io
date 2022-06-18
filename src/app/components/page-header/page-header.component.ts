c1868673a22e8981cbc54c2a71ddf2b512cd45a8
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-06-18 21:37:38 +0430

    style(pageHeader): fix the issue with page header icon size

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -5,7 +5,7 @@ import { NavigationRouteModel } from '../../models/navigationRoute.model';
     selector: 'app-page-header',
     template: `
         <header class="flex items-center mb-5 p-5 dark:bg-custom-yellow-500 bg-custom-blue-600 dark:text-custom-purple-900 text-yellow-500">
-            <mat-icon class="mx-5 flex items-center text-4xl">{{ pageHeaderInfo.routeIcon }}</mat-icon>
+            <mat-icon class="mx-5 flex items-center text-4xl h-full min-w-min">{{ pageHeaderInfo.routeIcon }}</mat-icon>
             <h1 class="m-0 text-4xl">{{ pageHeaderInfo.routeTitle }}</h1>
         </header>
     `,
