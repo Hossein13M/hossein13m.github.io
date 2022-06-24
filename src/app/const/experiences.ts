bd07f170d27210565aba360268cba4f6b079abf3
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-06-24 20:51:06 +0430

    chore(component): add mock experiences

---
+++ src/app/const/experiences.ts
@@ -0,0 +1,106 @@
+import { ExperienceModel } from '../models/experience.model';
+
+export const Experiences: Array<ExperienceModel> = [
+    {
+        company: {
+            name: 'AliBaba Travels',
+            logoUrl: '../../../assets/images/experience/temp-logo',
+            location: 'Tehran - Iran',
+            industry: 'Travel Arrangements',
+            website: 'https://www.alibaba.ir',
+        },
+        jobPosition: {
+            employmentType: 'Full-Time',
+            startDate: 'May 2022',
+            endDate: 'Present',
+            jobTitle: 'Front End Engineer',
+            jobDescription: 'Software Engineer',
+            jobRequiredSkills: ['JavaScript', 'TypeScript', 'Git', 'VueJS', 'NuxtJS', 'HTML', 'CSS', 'Sass', 'Linux', 'Tailwinds'],
+        },
+    },
+    {
+        company: {
+            name: 'Tamadon Investment Bank',
+            logoUrl: '../../../assets/images/experience/temp-logo',
+            location: 'Tehran - Iran',
+            industry: 'Financial Industry',
+            website: 'https://tamadonib.com/en',
+        },
+        jobPosition: {
+            employmentType: 'Part-Time',
+            startDate: 'March 2022',
+            endDate: 'Present',
+            jobTitle: 'Software Engineer',
+            jobDescription: 'Software Engineer',
+            jobRequiredSkills: ['JavaScript', 'TypeScript', 'Git', 'Angular', 'ReactJS', 'HTML', 'CSS', 'Sass', 'Linux', 'Tailwinds'],
+        },
+    },
+    {
+        company: {
+            name: 'AliBaba Travels',
+            logoUrl: '../../../assets/images/experience/temp-logo',
+            location: 'Tehran - Iran',
+            industry: 'Travel Arrangements',
+            website: 'https://www.alibaba.ir',
+        },
+        jobPosition: {
+            employmentType: 'Full-Time',
+            startDate: 'May 2022',
+            endDate: 'Present',
+            jobTitle: 'Front End Engineer',
+            jobDescription: 'Software Engineer',
+            jobRequiredSkills: ['JavaScript', 'TypeScript', 'Git', 'VueJS', 'NuxtJS', 'HTML', 'CSS', 'Sass', 'Linux', 'Tailwinds'],
+        },
+    },
+    {
+        company: {
+            name: 'AliBaba Travels',
+            logoUrl: '../../../assets/images/experience/temp-logo',
+            location: 'Tehran - Iran',
+            industry: 'Travel Arrangements',
+            website: 'https://www.alibaba.ir',
+        },
+        jobPosition: {
+            employmentType: 'Full-Time',
+            startDate: 'May 2022',
+            endDate: 'Present',
+            jobTitle: 'Front End Engineer',
+            jobDescription: 'Software Engineer',
+            jobRequiredSkills: ['JavaScript', 'TypeScript', 'Git', 'VueJS', 'NuxtJS', 'HTML', 'CSS', 'Sass', 'Linux', 'Tailwinds'],
+        },
+    },
+    {
+        company: {
+            name: 'AliBaba Travels',
+            logoUrl: '../../../assets/images/experience/temp-logo',
+            location: 'Tehran - Iran',
+            industry: 'Travel Arrangements',
+            website: 'https://www.alibaba.ir',
+        },
+        jobPosition: {
+            employmentType: 'Full-Time',
+            startDate: 'May 2022',
+            endDate: 'Present',
+            jobTitle: 'Front End Engineer',
+            jobDescription: 'Software Engineer',
+            jobRequiredSkills: ['JavaScript', 'TypeScript', 'Git', 'VueJS', 'NuxtJS', 'HTML', 'CSS', 'Sass', 'Linux', 'Tailwinds'],
+        },
+    },
+    {
+        company: {
+            name: 'AliBaba Travels',
+            logoUrl: '../../../assets/images/experience/temp-logo',
+            location: 'Tehran - Iran',
+            industry: 'Travel Arrangements',
+            website: 'https://www.alibaba.ir',
+        },
+        jobPosition: {
+            employmentType: 'Full-Time',
+            startDate: 'May 2022',
+            endDate: 'Present',
+            jobTitle: 'Front End Engineer',
+            jobDescription: 'Software Engineer',
+            jobRequiredSkills: ['JavaScript', 'TypeScript', 'Git', 'VueJS', 'NuxtJS', 'HTML', 'CSS', 'Sass', 'Linux', 'Tailwinds'],
+        },
+    },
+];
