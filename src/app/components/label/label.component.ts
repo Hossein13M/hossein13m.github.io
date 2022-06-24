0f29176a5127778e0052296e4c30c08d6e84cab9
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-06-24 18:16:36 +0430

    style(component): dynamic padding for label component

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-label',
     template: `
-        <div [class]="labelClass" class="p-2 flex items-center text-base">
+        <div [class]="labelClass" class="flex items-center text-base">
             <span class="px-1">{{ labelText }}</span>
             <mat-icon *ngIf="!!labelIcon" inline>{{ labelIcon }}</mat-icon>
         </div>
@@ -11,8 +11,8 @@ import { Component, Input } from '@angular/core';
 })
 export class LabelComponent {
     @Input() labelText: string = 'March 2020 - April 2021';
-    @Input() labelClass: string = 'rounded-full bg-custom-blue-400 text-custom-grey-200 flex-row-reverse';
-    @Input() labelIcon: string | null = 'settings';
+    @Input() labelClass: string = 'rounded-full p-1 bg-custom-purple-100 text-custom-grey-200 flex-row-reverse';
+    @Input() labelIcon!: string | null;
 
     constructor() {}
 }
