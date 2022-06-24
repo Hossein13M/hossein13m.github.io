83d6ea22a931e1c716fc58b7c75787631abb6deb
hossein mousavi <dev.hosseinmousavi@gmail.com>
2022-06-24 19:27:58 +0430

    chore(component): add image to label component

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -4,15 +4,24 @@ import { Component, Input } from '@angular/core';
     selector: 'app-label',
     template: `
         <div [class]="labelClass" class="flex items-center text-base">
+            <img *ngIf="imageSrc" [src]="imageSrc" [alt]="imageSrc" class="label-img" />
             <span class="px-1">{{ labelText }}</span>
             <mat-icon *ngIf="!!labelIcon" inline>{{ labelIcon }}</mat-icon>
         </div>
     `,
+    styles: [
+        `
+            .label-img {
+                max-height: 40px;
+            }
+        `,
+    ],
 })
 export class LabelComponent {
     @Input() labelText: string = 'March 2020 - April 2021';
     @Input() labelClass: string = 'rounded-full p-1 bg-custom-purple-100 text-custom-grey-200 flex-row-reverse';
-    @Input() labelIcon!: string | null;
+    @Input() labelIcon!: string;
+    @Input() imageSrc!: string;
 
     constructor() {}
 }
