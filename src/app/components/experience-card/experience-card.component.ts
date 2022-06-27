7a1b41ac2ccc472a1692fa5de0e2fdaaa4d65549
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-06-27 21:01:16 +0430

    feat(component): implement experience card component

---
+++ src/app/components/experience-card/experience-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CompanyModel, JobPositionModel } from '../../models/experience.model';
+import { Companies } from '../../const/experiences';
+
+@Component({
+    selector: 'app-experience-card',
+    templateUrl: './experience-card.component.html',
+    styleUrls: ['./experience-card.component.scss'],
+})
+export class ExperienceCardComponent implements OnInit {
+    @Input() job!: JobPositionModel;
+    public company!: CompanyModel;
+    constructor() {}
+
+    ngOnInit(): void {
+        this.company = this.getCompany(this.job.companyId);
+    }
+
+    private getCompany(companyId: number): CompanyModel {
+        return Companies.find((company) => company.companyId === companyId)!;
+    }
+}
