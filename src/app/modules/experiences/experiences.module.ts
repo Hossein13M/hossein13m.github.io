96e2e5ae17849a18095711845b611c6e15664e34
hossein <hossein13mousavi@gmail.com>
2022-06-27 10:08:15 +0430

    feat(experienceModule): add company component to experience page and enhance style

--- src/app/modules/experiences/experiences.module.ts
+++ src/app/modules/experiences/experiences.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ExperiencesComponent } from './experiences.component';
 import { ExperiencesRoutingModule } from './experiences.routing.module';
 import { ExperienceCardModule } from '../../components/experience-card/experience-card.module';
+import { CompanyCardModule } from '../../components/company-card/company-card.module';
 
 @NgModule({
     declarations: [ExperiencesComponent],
-    imports: [CommonModule, ExperiencesRoutingModule, ExperienceCardModule],
+    imports: [CommonModule, ExperiencesRoutingModule, ExperienceCardModule, CompanyCardModule],
 })
 export class ExperiencesModule {}
