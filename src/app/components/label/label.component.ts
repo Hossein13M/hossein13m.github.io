926240e907f3b9927bb2ef806cecf5c09187ac8c
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-06-27 21:47:12 +0430

    style(component): hover effects for label component

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-label',
     template: `
-        <div [class]="labelClass" class="flex items-center text-base rounded-full p-1 flex-row-reverse">
+        <div [class]="labelClass" class="flex items-center text-base rounded-full p-1 flex-row-reverse cursor-default">
             <img *ngIf="imageSrc" [src]="imageSrc" [alt]="imageSrc" class="label-img" />
             <span class="px-1">{{ labelText }}</span>
             <mat-icon *ngIf="!!labelIcon" inline>{{ labelIcon }}</mat-icon>
@@ -19,7 +19,8 @@ import { Component, Input } from '@angular/core';
 })
 export class LabelComponent {
     @Input() labelText: string = 'March 2020 - April 2021';
-    @Input() labelClass: string = 'dark:bg-custom-purple-100 dark:text-custom-grey-200 bg-custom-blue-400 text-custom-yellow-500';
+    @Input() labelClass: string =
+        'dark:bg-custom-purple-100 dark:text-custom-grey-200 dark:hover:bg-custom-yellow-500 dark:hover:text-custom-purple-900 bg-custom-blue-400 text-custom-yellow-500 hover:bg-custom-blue-600 hover:text-white hover:font-bold dark:hover:font-bold';
     @Input() labelIcon!: string;
     @Input() imageSrc!: string;
 
