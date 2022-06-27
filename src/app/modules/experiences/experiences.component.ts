1a6708045668182e31eaa54bdceb1a963dbef07f
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-06-27 21:42:15 +0430

    chore(component): remove template and style file for experience module

--- src/app/modules/experiences/experiences.component.ts
+++ src/app/modules/experiences/experiences.component.ts
@@ -4,8 +4,11 @@ import { Companies, Experiences } from '../../const/experiences';
 
 @Component({
     selector: 'app-experiences',
-    templateUrl: './experiences.component.html',
-    styleUrls: ['./experiences.component.scss'],
+    template: `
+        <div *ngFor="let job of jobs" class="grid grid-cols-1 py-3 my-2">
+            <app-experience-card class="xp-card" [job]="job"></app-experience-card>
+        </div>
+    `,
 })
 export class ExperiencesComponent {
     public companies: Array<CompanyModel> = Companies;
