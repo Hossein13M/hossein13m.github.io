a1636e1b0b87bc9f881cf7e2a5ead7f2dfb1bacf
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-08-08 18:51:36 +0430

    chore(navRoutes): partially implementation of about me section

--- src/app/const/navigationRoutes.ts
+++ src/app/const/navigationRoutes.ts
@@ -2,6 +2,7 @@ import { NavigationRouteModel } from '../models/navigationRoute.model';
 
 export const NavigationRoutes: Array<NavigationRouteModel> = [
     { routeTitle: 'Home!', routeUrl: '', routeIcon: 'home' },
+    // { routeTitle: 'About Me!', routeUrl: 'about-me', routeIcon: 'hail' },
     { routeTitle: 'Contact Me!', routeUrl: 'contact-me', routeIcon: 'phone_iphone' },
     { routeTitle: 'Experiences!', routeUrl: 'experiences', routeIcon: 'work_history' },
     { routeTitle: 'Contents!', routeUrl: 'contents', routeIcon: 'article' },
