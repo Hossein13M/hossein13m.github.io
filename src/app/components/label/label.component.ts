024838661bdf6bdb91379701cc721ddc9ae9e8d8
hossein <hossein13mousavi@gmail.com>
2022-08-21 10:45:13 +0430

    chore(labelComponent): remove default value for an input

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -18,9 +18,7 @@ import { Component, Input } from '@angular/core';
     ],
 })
 export class LabelComponent {
-    @Input() labelText: string = 'March 2020 - April 2021';
-    @Input() labelClass: string =
-        'dark:bg-custom-purple-100 dark:text-custom-grey-200 dark:hover:bg-custom-yellow-500 dark:hover:text-custom-purple-900 bg-custom-blue-400 text-custom-yellow-500 hover:bg-custom-blue-600 hover:text-white hover:font-bold dark:hover:font-bold';
+    @Input() labelText!: string;
     @Input() labelIcon!: string;
     @Input() imageSrc!: string;
     @Input() fontSize: string = '16px';
