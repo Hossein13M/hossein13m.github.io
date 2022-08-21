68a70e1337d4e068a3aeee1faefa4bdf22584cff
hossein <hossein13mousavi@gmail.com>
2022-08-21 10:44:52 +0430

    feat(labelComponent): add new input for label component

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
     template: `
         <div [class]="labelClass" class="flex items-center text-base rounded-full p-1 flex-row-reverse cursor-default">
             <img *ngIf="imageSrc" [src]="imageSrc" [alt]="imageSrc" class="label-img" />
-            <span class="px-1">{{ labelText }}</span>
+            <span class="px-1" [style.font-size]="fontSize">{{ labelText }}</span>
             <mat-icon *ngIf="!!labelIcon" inline>{{ labelIcon }}</mat-icon>
         </div>
     `,
@@ -23,6 +23,9 @@ export class LabelComponent {
         'dark:bg-custom-purple-100 dark:text-custom-grey-200 dark:hover:bg-custom-yellow-500 dark:hover:text-custom-purple-900 bg-custom-blue-400 text-custom-yellow-500 hover:bg-custom-blue-600 hover:text-white hover:font-bold dark:hover:font-bold';
     @Input() labelIcon!: string;
     @Input() imageSrc!: string;
+    @Input() fontSize: string = '16px';
+    @Input() labelClass: string =
+        'text-xs dark:bg-custom-purple-100 dark:text-custom-grey-200 dark:hover:bg-custom-yellow-500 dark:hover:text-custom-purple-900 bg-custom-blue-400 text-custom-yellow-500 hover:bg-custom-blue-600 hover:text-white hover:font-bold dark:hover:font-bold';
 
     constructor() {}
 }
