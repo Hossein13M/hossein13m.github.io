a047b8b64a7d11b1edf2f214f2b24886b05393d3
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-10-16 11:11:19 +0330

    Revert "chore(pageHeader): #MahsaAmini"
    This reverts commit 4dff99fd9d52d78936862c4e247d26fd6fac6f3f.

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -4,30 +4,11 @@ import { NavigationRouteModel } from '../../models/navigationRoute.model';
 @Component({
     selector: 'app-page-header',
     template: `
-        <header
-            class="flex items-center justify-between mb-5 p-3 sm:p-5 dark:bg-custom-yellow-500 bg-custom-blue-600 dark:text-custom-purple-900 text-yellow-500 "
-        >
-            <div class="flex items-center">
-                <mat-icon class="mx-5 flex items-center text-2xl sm:text-4xl h-full min-w-min">{{ pageHeaderInfo.routeIcon }}</mat-icon>
-                <h1 class="m-0 text-2xl sm:text-4xl">{{ pageHeaderInfo.routeTitle }}</h1>
-            </div>
-            <a href="https://twitter.com/search?q=%23mahsaAmini" target="_blank" class="flex justify-center my-0 sm:text-3xl font-bold">
-                <div class="hidden sm:flex">
-                    <span>Iranian Lives Matter</span>
-                    <span class="text-blue-500 mx-2">#MahsaAmini</span>
-                </div>
-                <span class="block sm:hidden text-xl text-blue-500 font-bold">#MahsaAmini</span>
-            </a>
+        <header class="flex items-center mb-5 p-3 sm:p-5 dark:bg-custom-yellow-500 bg-custom-blue-600 dark:text-custom-purple-900 text-yellow-500">
+            <mat-icon class="mx-5 flex items-center text-2xl sm:text-4xl h-full min-w-min">{{ pageHeaderInfo.routeIcon }}</mat-icon>
+            <h1 class="m-0 text-2xl sm:text-4xl">{{ pageHeaderInfo.routeTitle }}</h1>
         </header>
     `,
-    styles: [
-        `
-            .blood-background {
-                background: url('https://www.freepnglogos.com/uploads/blood-png/bloods-photos-pic-28.png') center;
-                background-size: cover;
-            }
-        `,
-    ],
 })
 export class PageHeaderComponent {
     @Input() pageHeaderInfo!: NavigationRouteModel;
