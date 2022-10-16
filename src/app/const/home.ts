82e62e4b0798a0228ea1c7b1564d33989b118acb
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-10-16 11:27:44 +0330

    Revert "chore(const): change header type effect items"
    This reverts commit a6c0fae2ca25c13a87c4916c982220c0874e3b37.

--- src/app/const/home.ts
+++ src/app/const/home.ts
@@ -7,11 +7,11 @@ export const HomeTitles: Array<HomeModel> = [
 ];
 
 export const HeaderTypeEffectItems: Array<string> = [
-    'I Record Podcast...',
-    'I Like to Know More About The universe...',
-    'I Build Web Applications...',
-    'I Write Technical Contents...',
-    'I Consult Teams To Grow...',
-    'I am trying to Contribute to Open-Source...',
-    'I Am A Full Time Learner...',
+    'I Record Podcast',
+    'I Like to Know More About Universe',
+    'I Build Web Applications.',
+    'I Write Technical Contents.',
+    'I Consult Teams To Grow.',
+    'I Contribute to Open-Source.',
+    'I Am A Full Time Learner.',
 ];
