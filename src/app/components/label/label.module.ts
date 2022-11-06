e18f0597ab838e284e890443dcd9682c538f53f9
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-11-06 15:17:27 +0330

    refactor(components): optimize imports

--- src/app/components/label/label.module.ts
+++ src/app/components/label/label.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LabelComponent } from './label.component';
 import { MatIconModule } from '@angular/material/icon';
+import { LabelComponent } from './label.component';
 
 @NgModule({
     declarations: [LabelComponent],
--- src/app/components/skill-card/skill-card.module.ts
+++ src/app/components/skill-card/skill-card.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SkillCardComponent } from './skill-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import { LevelBadgeModule } from '../level-badge/level-badge.module';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { SkillCardComponent } from './skill-card.component';
+import { LevelBadgeModule } from '../level-badge/level-badge.module';
 
 @NgModule({
     declarations: [SkillCardComponent],
