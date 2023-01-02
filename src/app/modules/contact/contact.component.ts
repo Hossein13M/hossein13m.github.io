4f826c369d9986da3627286cf6d740a6516d98e5
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-01-02 22:48:15 +0330

    feat(global): initialize transloco i18n

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
     selector: 'app-contact',
     template: `
         <div class="p-4 flex flex-col">
+            <h1 *transloco="let t; read: 'contact'">{{ t('hello') }}</h1>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-5">
                 <app-contact-link [contactLinkType]="'social'"></app-contact-link>
                 <app-contact-link [contactLinkType]="'openSource'"></app-contact-link>
