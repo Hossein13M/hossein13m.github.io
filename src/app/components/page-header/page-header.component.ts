6fdcb206ae1f90e13cab43644399c35124b223e1
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-06-06 00:10:55 +0330

    chore(pageHeader): remove symbol

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -11,19 +11,6 @@ import type { NavigationRouteModel } from '../../models/navigationRoute.model';
                 <mat-icon class="mx-2 flex items-center text-2xl sm:text-4xl h-full min-w-min">{{ pageHeaderInfo.routeIcon }}</mat-icon>
                 <h1 class="m-0 text-2xl font-bold sm:font-normal sm:text-4xl">{{ pageHeaderInfo.routeTitle }}</h1>
             </div>
-            <div class="flex items-center justify-center my-0 sm:text-3xl font-bold mt-2 sm:mt-0 sm:mr-5">
-                <a
-                    href="https://twitter.com/search?q=%23mahsaAmini"
-                    class="hidden sm:flex text-xl sm:text-3xl dark:text-custom-purple-500 font-black"
-                    target="_blank"
-                >
-                    <span class="dastan">#WomanLifeFreedom</span>
-                    <img src="../../../assets/images/womanLifeFreedom.png" alt="" width="40" class="ml-2" />
-                </a>
-                <a class="block sm:hidden" href="https://twitter.com/search?q=%23mahsaAmini">
-                    <img src="../../../assets/images/womanLifeFreedom.png" alt="" width="40" />
-                </a>
-            </div>
         </header>
     `,
 })
