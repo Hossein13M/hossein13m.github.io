92afc893c18fde92aeb576023c4e839f889ff858
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-06-08 18:30:19 +0330

    feat(global): add about me section

--- src/app/modules/about-me/about-me.module.ts
+++ src/app/modules/about-me/about-me.module.ts
@@ -4,9 +4,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { AboutMeComponent } from './about-me.component';
 import { AboutMeRoutingModule } from './about-me.routing.module';
+import { DividerModule } from '../../components/divider/divider.module';
 
 @NgModule({
     declarations: [AboutMeComponent],
-    imports: [CommonModule, AboutMeRoutingModule, MatCardModule, MatDividerModule],
+    imports: [CommonModule, AboutMeRoutingModule, MatCardModule, MatDividerModule, DividerModule],
 })
 export class AboutMeModule {}
