5508892271704df9f16626d78175dea962990d6a
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-06-08 13:45:49 +0330

    chore(homeConst): change experience time

--- src/app/const/home.ts
+++ src/app/const/home.ts
@@ -1,7 +1,7 @@
 import type { HomeModel } from '../models/home.model';
 
 export const HomeTitles: Array<HomeModel> = [
-    { title: '+5 Years', subtitle: 'IT Industry Experience' },
+    { title: '+7 Years', subtitle: 'IT Industry Experience' },
     { title: '+45', subtitle: 'Open-Source Contributions' },
     { title: '+600K', subtitle: 'My Contents Reach' },
 ];
