7373d368f2495408eeeeb907b83102fb4015a32e
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-12-24 02:36:31 +0330

    chore(global): change title

--- src/app/const/experiences.ts
+++ src/app/const/experiences.ts
@@ -57,7 +57,7 @@ export const Experiences: Array<JobPositionModel> = [
         employmentType: 'Full-Time',
         startDate: 'january 2021',
         endDate: 'Present',
-        jobTitle: 'Front End Developer / Front End Mentor',
+        jobTitle: 'Front End Engineer / Front End Mentor',
         jobDescription: [
             'As a part of Iran’s biggest travel agency I have Increased tour product convention rate from 1.5% to 4% by implementing the A/B test infrastructure plugin and applying it to the PLP and PDP product redesign',
             'Led the migration of the Tour product to the new website, resulting in a 10% performance boost by lazy loading images, using async components, and using lifecycle hooks properly',
