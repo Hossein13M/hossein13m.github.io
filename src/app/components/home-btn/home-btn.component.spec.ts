f2e290a3a5be4223853c533c1b7a7afe0a28cc60
Hossein Mousavi <hossein13mousavi@gmail.com>
2024-04-25 03:51:03 +0330

    feat(app): generalize home button and promote telegram channel

--- src/app/components/home-btn/home-btn.component.spec.ts
+++ src/app/components/home-btn/home-btn.component.spec.ts
@@ -1,17 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AdpBtnComponent } from './adp-btn.component';
+import { HomeBtnComponent } from './home-btn.component';
 
 describe('AdpBtnComponent', () => {
-    let component: AdpBtnComponent;
-    let fixture: ComponentFixture<AdpBtnComponent>;
+    let component: HomeBtnComponent;
+    let fixture: ComponentFixture<HomeBtnComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [AdpBtnComponent],
+            declarations: [HomeBtnComponent],
         }).compileComponents();
 
-        fixture = TestBed.createComponent(AdpBtnComponent);
+        fixture = TestBed.createComponent(HomeBtnComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
