4b0a909c0fd6d217899a08f85d74bec17648fd76
Hossein <dev.hosseinmousavi@gmail.com>
2024-09-12 01:14:57 +0200

    empty branch

--- src/app/models/snackbar.model.ts
+++
@@ -1,9 +0,0 @@
-export interface SnackbarModel {
-    classes: Array<string>;
-    message: string;
-}
-
-export interface SnackBar {
-    onSuccess: SnackbarModel;
-    onError: SnackbarModel;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    baseUrl: 'https://hmousavi.dev',
-    production: true,
-};
