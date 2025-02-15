f1bad1e2a23954e208bc99a9e1dd73f72f5ba6f1
Hossein Mousavi <hossein13mousavi@gmail.com>
2025-02-15 16:23:21 +0100

    feat(global): implement import alias for the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+import path from 'path';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
   target: 'static',
   srcDir: 'src/',
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
+    '@components': path.resolve(__dirname, 'src/components'),
+  },
 });
