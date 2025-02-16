cc89f6c299d9cb3fb9f77dde72355e88ec88aef4
Hossein Mousavi <hossein13mousavi@gmail.com>
2025-02-16 16:12:32 +0100

    feat(global): add tailwindCss to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
@@ -8,9 +9,13 @@ export default defineNuxtConfig({
   ssr: false,
   target: 'static',
   srcDir: 'src/',
+  css: ['~/assets/css/main.css'],
   alias: {
     '@': path.resolve(__dirname, 'src'),
     '@components': path.resolve(__dirname, 'src/components'),
     '@types': path.resolve(__dirname, 'src/types'),
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
